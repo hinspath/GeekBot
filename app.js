ec27e1a3de07c5b227ecdce7da6908d8c0c7f6bd
hinspath <54998533+hinspath@users.noreply.github.com>
2020-05-29 17:35:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Bot = require('./modules/bot');
 const process = require('process');
-const axios = require('axios').default;
+const axios = require('./plugins/axios').default;
 
 require('./modules/date-format');
 
