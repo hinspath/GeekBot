d9e7ee4fabdb731b3468aaa2afc39568a4116597
hinspath <54998533+hinspath@users.noreply.github.com>
2020-05-29 16:54:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ class Plugin extends Bot {
   }
   async run () {
     const now = new Date().Format('M/d h:m:s');
+    now.setHours(now.getHours() + 8);
     await this.sendImage('assets/geekbot.png');
     // 发送环境配置信息
     const CONF_DATA = `
